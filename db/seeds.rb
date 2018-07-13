f44fe78daf270b22b60ba1d1cb1d1b10cef72aa8
James Turello <jturello@yahoo.com>
2018-07-13 16:38:10 -0700

    add List seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 
+list_names = ["Groceries", "Jon's Wedding", "Mom's Birthday"]
+
+list_names.each do | list_name |
+  List.create(name: list_name)
+end
